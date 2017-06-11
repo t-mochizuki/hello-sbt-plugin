2fd18e150e5257df84ed81afc3e6d3477a98f9ee
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2017-06-11 13:01:53 +0900

    空ファイルを作成するコマンドを追加する closed #1

--- src/main/scala/com/github/t-mochizuki/HelloSbtPlugin.scala
+++ src/main/scala/com/github/t-mochizuki/HelloSbtPlugin.scala
@@ -1,14 +1,23 @@
 import sbt._
 import Keys._
 
+import java.io.{File, FileWriter}
+
 object HelloSbtPlugin extends AutoPlugin {
-  override lazy val projectSettings = Seq(commands ++= Seq(hello, world))
+  override lazy val projectSettings = Seq(commands ++= Seq(hello, world, createFile))
   lazy val hello = Command.command("hello") { state =>
     println("hello")
     state
   }
   lazy val world = Command.command("world") { state =>
     println("world")
+    state
+  }
+  lazy val createFile = Command.command("createFile") { state =>
+    val file = new File("./newFile")
+    val fileWriter = new FileWriter(file, false)
+    fileWriter.write("")
+    fileWriter.close()
     state
   }
 }
